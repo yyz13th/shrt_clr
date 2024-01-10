15cb8d2b43b64d84b17900d285e4ce693ade9277
yyz13th <136233684+yyz13th@users.noreply.github.com>
2024-01-10 14:56:14 +0300

    index for components

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,7 @@
 import CustomButton from "./Custombutton.component";
+import FilePicker from "./FilePicker.component";
+import AIPicker from "./AIPicker.component";
+import ColorPicker from "./ColorPicker.components";
+import Tab from "./Tab.component";
 
-export { CustomButton }
+export { CustomButton, FilePicker, AIPicker, ColorPicker, Tab }
